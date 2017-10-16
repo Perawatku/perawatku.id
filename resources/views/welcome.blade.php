25207cb6a4984c940e0bef94c298dce45542ec40
riyadh99 <phaseldaid>
2017-10-15 23:34:51 -0700

    ganting lo liak
    gfgfgg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Perawatku.ADe ganteng
+                    Perawatku. testtest
                 </div>
 
                 <div class="links">
