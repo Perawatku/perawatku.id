3609f87e48929fb479bdb04ebfa3d12f1b58af42
riyadh99 <phaseldaid>
2017-10-15 23:30:59 -0700

    ubah nama
    bla bla bla

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Perawatku.ADe
+                    Perawatku.ADe ganteng
                 </div>
 
                 <div class="links">
