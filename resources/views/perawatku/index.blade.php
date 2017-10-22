bcae615d261d5de31070ef4d145bdb2a856dc534
Arif Rahman <aerhaen@gmail.com>
2017-10-22 15:09:20 +0700

    halaman awal done

--- resources/views/perawatku/index.blade.php
+++ resources/views/perawatku/index.blade.php
@@ -1,85 +1,269 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<!DOCTYPE html>
+<html lang="en">
+    
+  
     <head>
+        
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Perawatku</title>
 
-        <title>Laravel</title>
+        <!-- Bootstrap -->
+        <link href="{{ url('/asset/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ url('/asset/css/animate.css') }}" rel="stylesheet">
+        <link href="{{ url('/asset/css/style.css') }}" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+  
     </head>
+  
     <body>
-        <div class="flex-center position-ref full-height">
+        
+        <nav class="navbar navbar-fixed-top navbar-menu">
+        
+            <div class="container">
+            
+                <div class="navbar-header">
+                
+                    <a href="" class="navbar-brand"><img src="{{ url('/asset/images/logo.png') }}"></a>
+                    
+                    <button class="navbar-toggle" data-target="#navbar" data-toggle="collapse">
+                    
+                        <div class="iconx">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Perawatku
-                </div>
+                            <div class="hamburger">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Home</a>
-                    <a href="{{ url('/listrawat') }}">Perawat</a>
-                    <a href="https://laravel-news.com">About Us</a>
-                    <a href="https://forge.laravel.com">FAQ</a>
-                    <a href="https://github.com/laravel/laravel">Hubungi Kami</a>
+
+
+                            </div>
+
+                        </div>
+                    
+                    </button>
+                
                 </div>
+                
+                <div class="collapse navbar-collapse navbar-collapse-full" id="navbar">
+                    
+                    <ul class="nav navbar-nav navbar-right nav-overlay">
+                        
+                        <a href="#" class="nav-close" data-target="#navbar" data-toggle="collapse"> Close</a>
+                        
+                        <li><hr></li>
+
+                        <li class="li-nav"><a href="">HOME</a></li>
+                        <li class="li-nav"><a href="{{ url('/listrawat') }}">PERAWATKU</a></li>
+                        <li class="li-nav"><a href="">ABOUT US</a></li>
+                        <li class="li-nav active"><a href="">FAQ</a></li>
+                        <li class="li-nav-profile"><a href="" class="profile-nav"><img class="img-circle img-nav" src="{{ url('/asset/images/profile-nav.png') }}" style="width:50px"></a><span class="name-nav">Jordan</span></li>
+
+                    </ul>
+
+                </div>
+            
             </div>
-        </div>
+        
+        </nav>
+        
+        
+        <section id="big-banner" class="big-banner">
+        
+            <div class="banner-content">
+
+                <div class="container">
+
+                    <div class="content">
+
+                        <h1>FAQ</h1>
+                        <p>Bootstrap ships with vanilla CSS,<br> but its source code utilizes.</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+            
+        </section>
+        
+        
+        <section id="faq">
+        
+            <div class="container">
+            
+                <div class="row">
+                    
+                    <div class="faq">
+                        
+                        <div class="col-sm-12">
+                    
+                            <h2 class="">1. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+                        
+                        </div>
+                        
+                        <div class="answer-faq">
+
+                            <h2 class="" style="color:#ef6452">1. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+
+                            <div class="col-sm-10 col-sm-offset-2">
+
+                            <blockquote>"Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS. CSSBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its"</blockquote>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="faq">
+                        
+                        <div class="col-sm-12">
+                    
+                            <h2 class="" >2. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+                        
+                        </div>
+                        
+                        <div class="answer-faq">
+
+                            <h2 class="" style="color:#ef6452">2. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+
+                            <div class="col-sm-10 col-sm-offset-2">
+
+                            <blockquote>"Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS. CSSBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its"</blockquote>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="faq">
+                        
+                        <div class="col-sm-12">
+                    
+                            <h2 class="">3. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+                        
+                        </div>
+                        
+                        <div class="answer-faq">
+
+                            <h2 class="" style="color:#ef6452">3. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+
+                            <div class="col-sm-10 col-sm-offset-2">
+
+                            <blockquote>"Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS. CSSBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its"</blockquote>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="faq">
+                        
+                        <div class="col-sm-12">
+                    
+                            <h2 class="">4. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+                        
+                        </div>
+                        
+                        <div class="answer-faq">
+
+                            <h2 class="" style="color:#ef6452">4. Bootstrap ships with vanilla CSS, but its source CSSBootstrap ships with vanilla CSS, but its source code utilizes ?</h2>
+
+                            <div class="col-sm-10 col-sm-offset-2">
+
+                            <blockquote>"Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS. CSSBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the sourceBootstrap ships with vanilla CSS, but its"</blockquote>
+
+                            </div>
+
+                        </div>
+                        
+                    </div>
+                
+                </div>
+            
+            </div>
+        
+        </section>
+        
+        <footer>
+        
+            <div class="container">
+            
+                <div class="footer">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-3 footer-col-4">
+                        
+                            <img src="{{ url('/asset/images/logo-footer.png') }}" style="width:150px">
+                            
+                            <p>Moment <span>Capturing Your Best Moment</span></p>
+                            
+                        </div>
+                        
+                        <div class="col-sm-3 footer-col-4">
+                        
+                            <h4>Site</h4>
+                            
+                            <ul>
+                            
+                                <li>Tentang Kami</li>
+                                <li>Kontak</li>
+                                <li>FAQ</li>
+                                <li>Syarat & Ketentuan</li>
+                                <li>Privasi</li>
+                                <li>Konfirmasi Pembayaran</li>
+                                <li>Order Tracker</li>
+                                
+                            </ul>
+                            
+                        </div>
+                        
+                        <div class="col-sm-3 footer-col-4">
+                        
+                            <h4>Hubungi kami</h4>
+                            
+                            <ul>
+                            
+                                <li>085121623526</li>
+                                <li>0751-7121521</li>
+                                <li></li>
+                                <li>Perawatku.id</li>
+                                <li>perawatku@gmail.com</li>
+                                
+                            </ul>
+                        
+                        </div>
+                        
+                        <div class="col-sm-3 footer-col-4">
+                        
+                            <p>Masukkan alamat email kamu untuk mendapatkan update informasi tentang perawatku.id</p>
+                            
+                        </div>
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+        
+        </footer>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="{{ url('/asset/js/jquery-3.1.1.min.js') }}"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="{{ url('/asset/js/bootstrap.min.js') }}"></script>
+        <script src="{{ url('/asset/js/custom.js') }}"></script>
+  
     </body>
+    
 </html>
